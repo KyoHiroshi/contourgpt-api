938b6b59405208cf15dde6bfad359c1ac9348656
mtariq <muhammad.tariq@redeye.com>
2024-04-02 17:23:43 +0100

    Complete segment optimization and dynamic segment building from user prompt

--- app.py
+++ app.py
@@ -1,71 +1,110 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 from contourgpt_v2 import enforce_json_structure
 import oracledb, json, sys
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all domains on all routes
 
-@app.route('/generateSegment', methods=['GET'])
+@app.route('/generateSegment', methods=['POST'])
 def generateSegment():
+
+    user_prompt = request.json.get('user_prompt')
+
     res = enforce_json_structure(system_prompt = '''You are to extract the segment information from the user's query and complete the segment information based on the format''',
-                    user_prompt = '''I want a oneoff segment named oldPeople which contains people over the age of fifty.''',
+                    user_prompt = user_prompt,
+                    # user_prompt = '''I want a oneoff segment named oldPeople which contains people over the age of fifty.''',
                     # user_prompt = '''I want a recurring segment named 3kids which contains people who have 3 kids.''',
                     # user_prompt = '''I want a oneoff segment named lowerClass which contains people who have an income of less than fifty thousand.''',
 
-                    # output_format = {
-                    #     "segmentName": "<segment name>",
-                    #     "segCalc": "<oneoff or recurring>",
-                    #     "ruleParam": "Specified rule parameter, type: str['Age', 'Income', 'Kids']",
-                    #     "ruleOperator": "Symbol for operator, type: str['=', '>=', '<=']",
-                    #     "operatorText": "Operator description, str['is equal to', 'greater than or equal to', 'less than or equal to']",
-                    #     "sentenceText": "Include People with an <ruleParam> value which  <operatorText>  <value>",
-                    #     "value": "Value supplied by user, type: int"
-                    #     }
-
                     output_format = {
-                        "segmentID": "null",
                         "segmentName": "<segment name>",
-                        "calculateWhen": "1",
                         "segCalc": "<oneoff or recurring>",
-                        "segDaysToCalc": "null",
-                        "segStructure": "(S1=M)",
                         "segCalcType": "<oneoff or recurring>",
-                        "rules": [
-                          {
-                              "include": "true",
-                              "ruleID": "1",
-                              "ruleOperator": "[=>, =, <=]",
-                              "ruleParam": "<Rule Parameter, type: str['Age', 'Income', 'Kids']>",
-                              "ruleGroupRecord": {
-                                  "text": "Person Attributes",
-                                  "value": "1"
-                              },
-                              "ruleParamRecord": {
-                                  "text": "<Rule Parameter>",
-                                  "value": "<Rule Parameter>"
-                              },
-                              "ruleValue": "<ruleValue, type: int>",
-                              "sentenceText": "Include People with an <Rule Parameter> which  is greater than or equal to  <value>",
-                              "ruleOperatorRecord": {
-                                  "text": "is greater than or equal to",
-                                  "value": "greater_than_or_equal",
-                                  "operatorAny": "false",
-                                  "operatorSymbol": ">="
-                              },
-                              "ruleValueRecord": {
-                                  "value": "<ruleValue>",
-                                  "text": "<ruleValue>"
-                              },
-                              "ruleType": "type:1",
-                              "ruleTypeRecord": {
-                                  "text": "<Rule Parameter>",
-                                  "value": "type:1"
-                              }
-                            }
-                          ]
+                        "ruleParam": "Specified rule parameter, type: str['Age', 'Income', 'Kids']",
+                        "ruleOperator": "Symbol for operator, type: str['=', '>=', '<=']",
+                        "operatorText": "Operator description, str['is equal to', 'greater than or equal to', 'less than or equal to']",
+                        "sentenceText": "Include People with an <ruleParam> value which  <operatorText>  <value>",
+                        "ruleValue": "Value supplied by user, type: int"
                         }
                     )
-    return jsonify(res)
-    print(res)
+    # print(res)
+
+    match res['ruleOperator']:
+        case "=":
+            ruleOperatorRecord = {
+                "ruleOperatorRecord": {
+                    "text": "is equal to",
+                    "value": "exact_match",
+                    "operatorAny": False,
+                    "operatorSymbol": "="
+                }
+            }
+            res.update(ruleOperatorRecord)
+        case ">":
+            res['ruleOperator'] = ">="
+            ruleOperatorRecord = {
+                "ruleOperatorRecord": {
+                    "text": "is greater than or equal to",
+                    "value": "greater_than_or_equal",
+                    "operatorAny": False,
+                    "operatorSymbol": ">="
+                }
+            }
+            res.update(ruleOperatorRecord)
+        case "<":
+            res['ruleOperator'] = "<="
+            ruleOperatorRecord = {
+                "ruleOperatorRecord": {
+                    "text": "is less than or equal to",
+                    "value": "less_than_or_equal_to",
+                    "operatorAny": False,
+                    "operatorSymbol": "<="
+                }
+            }
+            res.update(ruleOperatorRecord)
+        case _:
+            print("Fix this later")
+
+    segStruc = {
+                    "segmentID": None,
+                    "segmentName": res['segmentName'],
+                    "calculateWhen": "1",
+                    "segCalc": res['segCalc'],
+                    "segDaysToCalc": None,
+                    "segStructure": "(S1=M)",
+                    "segCalcType": res['segCalcType'],
+                    "rules": [
+                        {
+                            "include": True,
+                            "ruleID": "1",
+                            "ruleOperator": res['ruleOperator'],
+                            "ruleParam": res['ruleParam'].upper(),
+                            "ruleGroupRecord": {
+                                "text": "Person Attributes",
+                                "value": "1"
+                            },
+                            "ruleParamRecord": {
+                                "text": res['ruleParam'],
+                                "value": res['ruleParam'].upper()
+                            },
+                            "ruleValue": res['ruleValue'],
+                            "sentenceText": f"Include People with an {res['ruleParam']} which  {res['ruleOperatorRecord']['text']}  {res['ruleValue']}",
+                            "ruleOperatorRecord": res['ruleOperatorRecord'],
+                            "ruleValueRecord": {
+                                "value": res['ruleValue'],
+                                "text": res['ruleValue']
+                            },
+                            "ruleType": "type:1",
+                            "ruleTypeRecord": {
+                                "text": res['ruleParam'],
+                                "value": "type:1"
+                            }
+                        }
+                    ]
+                }
+
+    return jsonify(segStruc)
 
 def execute(sql, mode, **kwargs):
     try:
@@ -98,6 +137,7 @@ def execute(sql, mode, **kwargs):
 @app.route('/getOracleData', methods=['GET'])
 def getSegmentRules():
     sql = "SELECT ruleid, field, field_name, operator, operator_name FROM segment_rules WHERE ruleid IN (1, 8, 9)"
+    # sql = "SELECT ruleid, field, field_name, operator, operator_name FROM segment_rules WHERE field_name = (Insert Rule Parameter)"
     rows = execute(sql, 'Q')
 
     # Convert data into list of dictionaries
